fd4c5ca0248e537e1cf08f087c34a732d8a34964
blamas <blamas@asjservicios.com>
2024-06-09 13:42:29 -0300

    registro del usuario

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+    id: number;
+    nombre: string;
+    apellido: string;
+    email: string;
+    telefono: string; 
+    fechaNacimiento:string ;
+    dni: string;
+    genero: string;
+    usuario: string;
+    cobertura:string ;
+}
+  
+
