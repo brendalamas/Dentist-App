ef02ce0995ad28066c9e00f44cbf37d53c76c2dd
Agustin_Ochoa <8agustin00@gmail.com>
2024-06-09 22:21:05 -0300

    Se genera la vista paciente

---
+++ src/app/components/vistapaciente/vistapaciente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vistapaciente',
+  standalone: true,
+  imports: [],
+  templateUrl: './vistapaciente.component.html',
+  styleUrl: './vistapaciente.component.css'
+})
+export class VistapacienteComponent {
+
+}
