09d49a7ab88b4a4d109b6f2582c9d5bc864d60d2
blamas <blamas@asjservicios.com>
2024-06-11 13:05:09 -0300

    se agrega navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:HomeComponent},
+    {path:'home', component:HomeComponent},
+    {path:'login', component:LoginComponent},
+    {path:'register', component:RegisterComponent},
+    {path:'calendar', component:RegisterComponent},
+    {path:'**', redirectTo:''},
+];
